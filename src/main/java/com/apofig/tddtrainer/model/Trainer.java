df77fe5ada7877e1b0aade39441347365e167a93
Oleksandr Baglai <apofig@gmail.com>
2014-01-26 20:33:14 +0200

    shouldSecondTaskWhenResolveFirst

--- src/main/java/com/apofig/tddtrainer/model/Trainer.java
+++ src/main/java/com/apofig/tddtrainer/model/Trainer.java
@@ -7,7 +7,16 @@ package com.apofig.tddtrainer.model;
  */
 public class Trainer {
 
+    private Solver solver;
+
     public String getTask() {
+        if (solver != null) {
+            return "1+2=?";
+        }
         return "1+1=?";
+    }
+
+    public void update(Solver solver) {
+        this.solver = solver;
     }
 }
--- src/test/java/com/apofig/tddtrainer/model/TrainerTest.java
+++ src/test/java/com/apofig/tddtrainer/model/TrainerTest.java
@@ -1,8 +1,12 @@
 package com.apofig.tddtrainer.model;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * User: sanja
@@ -11,13 +15,37 @@ import static junit.framework.Assert.assertEquals;
  */
 public class TrainerTest {
 
+    private Trainer trainer;
+    private Solver solver;
+
+    @Before
+    public void setup() {
+        trainer = new Trainer();
+        solver = mock(Solver.class);
+    }
+
+    private void assertCurrentTask(String expected) {
+        assertEquals(expected, trainer.getTask());
+    }
+
+    private void shouldSolver(String value) {
+        when(solver.solve(anyString())).thenReturn(value);
+    }
+
     @Test
     public void shouldFirstTestFromListWhenStart() {
-        Trainer trainer = new Trainer();
+        assertCurrentTask("1+1=?");
+    }
 
-        String task = trainer.getTask();
+    @Test
+    public void shouldSecondTaskWhenResolveFirst() {
+        assertCurrentTask("1+1=?");
 
-        assertEquals("1+1=?", task);
+        shouldSolver("2");
+
+        trainer.update(solver);
+
+        assertCurrentTask("1+2=?");
     }
 
 }
